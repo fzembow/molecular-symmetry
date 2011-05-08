22925a0d7be858434e52b8b091d74bf79bc27f86
Filip Zembowicz <fzembow@gmail.com>
2011-05-07 20:00:07 -0400

    updated hadoop run script that works on a cluster

--- run_hadoop.sh
+++ run_hadoop.sh
@@ -1,10 +1,7 @@
 
-# check for correct arguments
-if [ $# -neq 1 ]
-then
-echo "usage: ./run_hadoop.sh (input filenames file)"
-exit 1
-fi
+# remove existing output, if any
+hadoop fs -rmr hadoop_output/
+rm -rf hadoop_output/
 
 # get reference to hadoop streaming jar
 export SJAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2*.jar
@@ -13,5 +10,7 @@ export SJAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2*.jar
 rm -rf hadoop_output/
 
 # spin off hadoop job
-hadoop jar $SJAR -input $1 -output hadoop_output/ -mapper $(pwd)/mapper.py -numReduceTasks 0 -file mapper.py -file extract.py
+hadoop jar $SJAR -input urls-full.txt -output hadoop_output/ -mapper $(pwd)/mapper.py -reducer $(pwd)/reducer.py -numReduceTasks 500 -file mapper.py -file extract.py -file reducer.py
 
+# retrieve results
+hadoop fs -copyToLocal hadoop_output/ hadoop_output/
